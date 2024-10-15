d637a0d1200f5e9a3060385ae5e8a7d8c415b4c9
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-15 23:33:20 +0500

    ULTRA VERSION

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, usedPrefix, isOwner }) => {
 VERSION:3.0
 N:;Qasim;;;
 FN:Qasim Ali
-ORG:Qasim Org
+ORG:GlobalTechInfo
 TITLE:Owner
-item1.TEL;waid=255734980103:255734980103
+item1.TEL;waid=923444844060:923444844060
 item1.X-ABLabel:Owner
 X-WA-BIZ-DESCRIPTION:Owner of the Bot
 X-WA-BIZ-NAME:Qasim Ali
