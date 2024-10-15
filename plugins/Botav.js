408ab9dbaa7d50406815dffee1762ef48f14acaa
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-15 23:55:53 +0500

    ULTRA VERSION

--- plugins/Botav.js
+++ plugins/Botav.js
@@ -1,23 +1,17 @@
 let handler = async (m, { conn }) => {
-  let user = global.db.data.users[m.sender]
-  let name = conn.getName(m.sender)
-  let taguser = '@' + m.sender.split('@s.whatsapp.net')[0]
-  let av = `./jusorts/${pickRandom(['qasim', 'global'])}.mp3`
+  let user = global.db.data.users[m.sender];
+  let name = conn.getName(m.sender);
+  let taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+  let av = `./assets/${pickRandom(['qasim', 'global'])}.mp3`;
 
+  conn.sendButton(m.chat, `*HOLA FROM BOT GLOBAL*\n        Morning or Evening\n\n @${m.sender.split('@')[0]}     \n\n*You called me what is your problem bro?* `.trim(), igfg, null, [['OWNER HELP', '.grp'], ['GET SC', '.repo']], m, { mentions: [m.sender] });
+  conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
+}
 
-conn.sendButton(m.chat, `*HOLA FROM BOT GLOBAL*      
-    morning or evening @${m.sender.split('@')[0]} 
-    You called me what is youre problem bro? 😇
-  `.trim(), igfg, null, [['OWNER HELP', '.grp'],['GET SC', '.repo']] , m, { mentions: [m.sender] })
-  
-conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-} 
-
-handler.customPrefix = /^(bot|qasim)$/i
-handler.command = new RegExp()
-
-export default handler
+handler.customPrefix = /^(bot|qasim)$/i;
+handler.command = new RegExp();
+export default handler;
 
 function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
+  return list[Math.floor(list.length * Math.random())];
 }
