5c69c57d39ee802cc3d558b8a387bd3062c78201
Vajiramd1 <ddismi13@gmail.com>
2024-10-16 20:06:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 }
 
 //global.pairingNumber = "92xxx_number" //put your bot number here
-global.botNumber = '923051391007' //put your bot number here
+global.botNumber = '94710136994' //put your bot number here
 
-global.mods = ['923444844060', '923051391007']
-global.prems = ['923444844060', '923051391007']
-global.allowed = ['923444844060', '923051391007']
+global.mods = ['94710136994']
+global.prems = ['94710136994']
+global.allowed = ['94710136994', '94710136994']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
