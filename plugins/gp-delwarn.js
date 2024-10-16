718debf19c92a2f6a4e55d1035c6e90a153f6850
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-16 13:22:18 +0500

    ULTRA VERSION

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -1,26 +1,24 @@
-
-let handler = async (m, { conn, args, groupMetadata}) => {
-        let who
-        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-        else who = m.chat
-        if (!who) throw `✳️ ${mssg.noMention}`
-        if (!(who in global.db.data.users)) throw `✳️ ${mssg.userDb}`
-       let warn = global.db.data.users[who].warn
-       if (warn > 0) {
-         global.db.data.users[who].warn -= 1
-         m.reply(`⚠️ *${mssg.delwarn.toUpperCase()}*
+let handler = async (m, { conn, args, groupMetadata }) => {
+  let who
+  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+  else who = m.chat
+  if (!who) throw `✳️ Tag or mention someone`
+  if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+  let warn = global.db.data.users[who].warn
+  if (warn > 0) {
+    global.db.data.users[who].warn -= 1
+    m.reply(`⚠️ *DELWARN*
          
-▢ ${mssg.warns}: *-1*
-▢ ${mssg.warns} ${mssg.total}: *${warn - 1}*`)
-         m.reply(`✳️ ${mssg.delWarnUser} *${warn - 1}*`, who)
-         } else if (warn == 0) {
-            m.reply(`✳️ ${mssg.warnNan}`)
-        }
-
+▢ Warns: *-1*
+▢ Warns total: *${warn - 1}*`)
+    m.reply(`✳️ An admin lowered their warning, now you have *${warn - 1}*`, who)
+  } else if (warn == 0) {
+    m.reply('✳️ The user has no warning')
+  }
 }
 handler.help = ['delwarn @user']
 handler.tags = ['group']
-handler.command = ['delwarn', 'unwarn'] 
+handler.command = ['delwarn', 'unwarn']
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
