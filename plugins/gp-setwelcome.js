5beaf07a92f2bacf5c566def027fcd03144223f4
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-16 13:05:38 +0500

    ULTRA VERSION

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -1,15 +1,16 @@
-//import db from '../lib/database.js'
-
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].sWelcome = text
-    m.reply(`✅ ${mssg.welMsgOn}`)
-  } else throw `✳️ ${mssg.welMsg}`
+    global.db.data.chats[m.chat].sWelcome = text;
+    m.reply(`✅ Welcome message updated successfully!`);
+  } else {
+    throw `✳️ Please provide a new welcome message.`;
+  }
 }
-handler.help = ['setwelcome']
-handler.tags = ['group']
-handler.command = ['setwelcome'] 
-handler.admin = true
-handler.owner = false
 
-export default handler
+handler.help = ['setwelcome'];
+handler.tags = ['group'];
+handler.command = ['setwelcome'];
+handler.admin = true;
+handler.owner = false;
+
+export default handler;
