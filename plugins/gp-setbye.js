61776b65b6e5a05e11ea4024bf7e407649a3f1cf
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-16 13:13:11 +0500

    ULTRA VERSION

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -1,15 +1,17 @@
 //import db from '../lib/database.js'
-
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].sBye = text
-    m.reply(`✅ ${mssg.leaMsgOn}`)
-  } else throw `✳️ ${mssg.leaMsg}`
+    global.db.data.chats[m.chat].sBye = text;
+    m.reply(`✅ Goodbye message updated successfully!`);
+  } else {
+    throw `✳️ Please provide a new goodbye message.`;
+  }
 }
-handler.help = ['setbye <text>']
-handler.tags = ['group']
-handler.command = ['setbye'] 
-handler.admin = true
-handler.owner = false
 
-export default handler
+handler.help = ['setbye <text>'];
+handler.tags = ['group'];
+handler.command = ['setbye'];
+handler.admin = true;
+handler.owner = false;
+
+export default handler;
