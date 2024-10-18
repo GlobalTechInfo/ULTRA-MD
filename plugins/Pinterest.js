2eba8ec53be0175383abfc18630b768c3b12fb20
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-18 13:47:13 +0500

    Create Pinterest.js

---
+++ plugins/Pinterest.js
@@ -0,0 +1,29 @@
+import pkg from 'nayan-media-downloader';
+const { pintarest } = pkg;
+
+const handler = async (m, { conn, args }) => {
+  if (!args[0]) throw `✳️ Enter the Pinterest link next to the command`;
+  if (!args[0].match(/(pinterest\.com\/pin\/|pin\.it\/)/gi)) throw `❌ Link incorrect`;
+  m.react('⏳');
+
+  try {
+    const url = args[0];
+    let data = await pintarest(url);
+    const { download, fileName, mimetype } = data;
+    
+    let caption = `≡ *Pinterest DL*\n▢ *Number:* ${fileName}\n▢ *Type:* ${mimetype}`.trim();
+    
+    await conn.sendFile(m.chat, download, fileName, caption, m, false, { mimetype });
+    m.react('✅');
+  } catch (error) {
+    console.error('Error downloading from Pinterest:', error);
+    await m.reply('⚠️ An error occurred while processing the request. Please try again later.');
+    m.react('❌');
+  }
+};
+
+handler.help = ['pinterest <url>'];
+handler.tags = ['downloader'];
+handler.command = ['pinterest', 'pin'];
+
+export default handler;
