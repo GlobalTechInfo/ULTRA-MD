8ece13832dd1b3bfa8b843a5f8a9e67fc2cf4b91
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-22 01:28:37 +0500

    ULTRA VERSION

--- plugins/devotional-mahadev.js
+++ plugins/devotional-mahadev.js
@@ -1,5 +1,5 @@
 
-import fetch from 'node-fetch'
+/* import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
 
@@ -8,8 +8,8 @@ let endpoint = `https://shizoapi.onrender.com/api/devotional/mahadev?apikey=shiz
 const response = await fetch(endpoint);
 if (response.ok) {
       const imageBuffer = await response.buffer();
-     /* await conn.sendButton(m.chat, imageBuffer, 'shizo.techie.err.png', msg, m, null, rpig);
-  .trim(), igfg, null, [['NEXT', '.shiva'],['GET SC', '.repo']] , m, { mentions: [m.sender] })*/
+     await conn.sendButton(m.chat, imageBuffer, 'shizo.techie.err.png', msg, m, null, rpig);
+  .trim(), igfg, null, [['NEXT', '.shiva'],['GET SC', '.repo']] , m, { mentions: [m.sender] })
   
      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m, null, rpig);
     } else {
@@ -21,3 +21,4 @@ handler.tags = ['images']
 handler.help = handler.command = ['mahadev', 'shiva']
 
 export default handler
+*/
