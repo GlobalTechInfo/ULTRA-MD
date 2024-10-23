a4376f20562a3426a8115bb3ba88ca5fa58a298d
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-23 23:31:15 +0500

    ULTRA VERSION

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,16 +1,22 @@
-import db from '../lib/database.js'
+import db from '../lib/database.js';
+import { execSync } from 'child_process';
+import fs from 'fs';
 
-import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
+  // Ensure that the command is executed by the owner
   if (conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-    conn.reply(m.chat, stdout.toString(), m)
+    // Execute the git pull command
+    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+    // Reload all plugins
+    fs.readdirSync('plugins').forEach(v => global.reload('', v));
+    // Reply with the output of the git command
+    conn.reply(m.chat, stdout.toString(), m);
   }
-}
-handler.help = ['update']
-handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed']
-handler.rowner = true
+};
 
-export default handler
+handler.help = ['update'];
+handler.tags = ['owner'];
+handler.command = ['update', 'actualizar', 'fix', 'fixed'];
+handler.rowner = true;
+
+export default handler;
