0a7fc461c0dab1d0fe199421b29c175207d4cad4
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-31 00:35:47 +0500

    ULTRA VERSION

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 import './config.js'; 
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 import path, { join } from 'path'
