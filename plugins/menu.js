5c5b298085f6bb866093c9fa8317fc320f80a95d
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-11-02 02:12:53 +0500

    ULTRA VERSION

--- plugins/menu.js
+++ plugins/menu.js
@@ -39,7 +39,7 @@ let handler = async (m, { conn, usedPrefix }) => {
                         text: "Use The Below Buttons"
                     }),
                     header: proto.Message.InteractiveMessage.Header.create({
-                        ...(await prepareWAMessageMedia({ image: { url: 'https://i.imgur.com/IfZsOhf.jpeg' } }, { upload: conn.waUploadToServer })),
+                        ...(await prepareWAMessageMedia({ image: { url: './assets/Ultra.jpg' } }, { upload: conn.waUploadToServer })),
                         title: null,
                         subtitle: null,
                         hasMediaAttachment: false
