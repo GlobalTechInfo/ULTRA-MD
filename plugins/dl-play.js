01a70f5915a6c8aa3fabb0ece829e718af13e9b8
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-11-01 23:26:14 +0500

    ULTRA VERSION

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -2,30 +2,35 @@ import yts from 'yt-search';
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `✳️ Example: *${usedPrefix + command}* Lil Peep hate my life`;
-    let res = await yts(text);
-    let vid = res.videos[0];
-    if (!vid) throw `✳️ Video/Audio not found`;
     
-    let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid;
-    m.react('🎧');
-    
-    let play = `
-≡ *FG MUSIC*
+    try {
+        let res = await yts(text);
+        let vid = res.videos[0];
+        
+        if (!vid) throw `✳️ Video/Audio not found`;
+
+        let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid;
+        m.react('🎧');
+
+        let play = `
+≡ *ULTRA-MD MUSIC*
 ┌──────────────
-▢ 📌 *Title:* ${vid.title}
-▢ 📆 *Uploaded:* ${vid.ago}
-▢ ⌚ *Duration:* ${vid.timestamp}
-▢ 👀 *Views:* ${vid.views.toLocaleString()}
+▢ 📌 *Title:* ${title}
+▢ 📆 *Uploaded:* ${ago}
+▢ ⌚ *Duration:* ${timestamp}
+▢ 👀 *Views:* ${views.toLocaleString()}
 └──────────────`;
 
-    await conn.sendButton(m.chat, play, mssg.ig, null, [
-        ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
-        ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
-    ], m, { mentions: [m.sender] });
+        await conn.sendButton(m.chat, play, null, null, [
+            ['🎶 MP3', `${usedPrefix}ytmp3 ${url}`],
+            ['🎥 MP4', `${usedPrefix}ytmp4 ${url}`]
+        ], m, { mentions: [m.sender] });
 
-    // Optionally send the audio file if needed
-    // const av = `./assets/${pickRandom(['qasim', 'global'])}.mp3`;
-    // await conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true });
+    } catch (error) {
+        console.error('Error in handler:', error);
+        // You can choose to notify the user if necessary
+        throw 'An error occurred while processing your request.';
+    }
 };
 
 handler.help = ['play'];
