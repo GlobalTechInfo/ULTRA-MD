e6a41873ae4944d19eec159e942faf61b18b1adc
STRIKERBOY <110353280+Devenlee350@users.noreply.github.com>
2024-11-05 00:35:51 +0200

    Update dl-playlist.js

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -29,7 +29,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
         });
     }
 
-    await conn.sendList(m.chat, '  ≡ *ULTRA-MD MUSIC*🔎', `\n 📀 Results for:\n *${text}*`, `Click Here`, ytres[0].image, listSections, m);
+    await conn.sendList(m.chat, '  ≡ *ULTRA-MD ◑ MUSIC*🔎', `\n 📀 Results for:\n *${text}*`, `Click Here`, ytres[0].image, listSections, m);
 };
 
 handler.help = ['play2'];
