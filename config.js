c3c4259a1fed3b5a38e0146aa54e00f72beabda4
Emmanuelkiplagat <trenchkidliltjay07@gmail.com>
2024-11-08 00:10:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.botNumber = '923051391007' //replace your bot number here
+global.botNumber = '254718842235' //replace your bot number here
 
 global.mods = []
 global.prems = []
