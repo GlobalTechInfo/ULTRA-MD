dc4b686bd5b65c0bcdd57988342660f437986f7b
zainzhxm1 <hdzhxm@gmail.com>
2024-11-18 02:51:05 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
 global.pairingNumber = process.env.BOT_NUMBER = 'your number';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID = 'session id';  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID = 'jj';  // put your session id here
 
 global.mods = []
 global.prems = []
