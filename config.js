adb7fb0bcd883894d030fc6f44d5e5906a05f154
chokoleeanmo <156728632+chokoleeanmo@users.noreply.github.com>
2024-11-23 06:03:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '2349134673563';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || 'your number';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '2349134673563';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
