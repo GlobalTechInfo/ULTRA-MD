793ebb64c2346abc02fee34c5241e873fd875687
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-12-03 12:29:04 +0500

    Update dl-igstory.js

--- plugins/dl-igstory.js
+++ plugins/dl-igstory.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import pkg from 'nayan-media-downloader';
+import pkg from 'nayan-video-downloader';
 const { instagram } = pkg;
 
 const fetchWithRetry = async (url, options, retries = 3) => {
